aa0e27f645ace25f56f7b8397e9bcfadb077185f
はる陽 <emaildai27@gmail.com>
2025-03-06 08:59:25 +0700

    delete build file

--- test/keypad/keypad.c
+++ test/keypad/keypad.c
@@ -2,8 +2,8 @@
 
 int main()
 {
-    const uint PIN_COLUMNS[4] = {9, 10, 11, 12};
-    const uint PIN_ROWs[4] = {8, 7, 6, 5};
+    const uint PIN_COLUMNS[4] = {5, 6, 7, 8};
+    const uint PIN_ROWs[4] = {13, 14, 15, 16};
 
     for (int index = 0; index < 4; index++)
         {
@@ -13,12 +13,12 @@ int main()
             gpio_init(column);
             gpio_init(row);
         
-            gpio_set_dir(column, false);
+            gpio_set_dir(column, GPIO_IN);
             gpio_pull_down(column);
-            gpio_set_dir(row, true);
+            gpio_set_dir(row, GPIO_OUT);
         
-            gpio_put(column, false);
-            gpio_put(row, true);
+            //gpio_put(column, false);
+            //gpio_put(row, true);
         }
 
     gpio_init(PICO_DEFAULT_LED_PIN);
@@ -32,23 +32,20 @@ int main()
         for (int row = 0; row < 4; row++)
         {
             gpio_put(PIN_ROWs[row], true);
-            sleep_ms(4);
+            sleep_ms(1);
 
-            for (int column; column < 4; column++)
+            for (int column = 0; column < 4; column++)
             {
-                if(gpio_get(column))
+                if(gpio_get(PIN_COLUMNS[column]))
                 {
-                    while (gpio_get(column))
-                    {
-                        sleep_ms(4);
-                    }
                     tick = !tick;
                     gpio_put(PICO_DEFAULT_LED_PIN, tick);
-                    sleep_ms(1000);
+                    sleep_ms(300);
                 }
             }
             gpio_put(PIN_ROWs[row], false);
         }
     }
     gpio_put(PICO_DEFAULT_LED_PIN, false);
+    return 0;
 }
